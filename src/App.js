7ca08f426c389845925f3fb14defecd4055b1ed0
SaiKiran <saikiranchatlapalli84@gmail.com>
2022-05-02 09:58:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { UserContext } from "./Components/UserContext";
+import { Home } from "./Components/Home";
+import { AddUser } from "./Components/AddUser";
+import { Navbar } from "./Components/Navbar";
+import { Edit } from "./Components/Edit";
+import { Profile } from "./Components/Profile";
+import { View } from "./Components/View";
+import { NotFound } from "./Components/NotFound";
+import "./App.css";
 
-function App() {
+export default function App() {
+  //created to useState
+  //one for user
+  const [user, setUser] = useState({
+    Firstname: "",
+    Lastname: "",
+    Empid: "",
+    City: ""
+  });
+  //second for list of users
+  const [users, setUsers] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={{ user, setUser, users, setUsers }}>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/adduser" element={<AddUser />} />
+        <Route exact path="/edit/:id" element={<Edit />} />
+        <Route exact path="/profile/:id" element={<Profile />} />
+        <Route exact path="/view/:id" element={<View />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </UserContext.Provider>
   );
 }
-
-export default App;
